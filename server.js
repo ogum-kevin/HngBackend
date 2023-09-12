031ef58070793ef7193974622f9daf547be14caf
Nivek-Mugo <83510916+ogum-kevin@users.noreply.github.com>
2023-09-12 11:08:01 +0300

    Updated the server and added routes and database

--- server.js
+++ server.js
@@ -1,43 +1,17 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const dbConnection= require('./db.js')
+const {notFound} = require('./Utils/errorHandler.js')
+const userRoute= require('./Routes/UserRoutes.js')
 
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
 
-
-/* Helper function */
-
-function dayOfTheWeek(day) {
-    const days = ['Sunday', 'Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    return days[day]
-}
-
-/* */
-
-app.get('/api',async(req,res) =>{
-    const slackName = req.query['slack_name'];
-    const  track = req.query['track'];
-    const parts = new Date().toISOString().split(".");
-
-    const UTC_time= parts[0]+ "Z"
-    
-    const getDayToday = new Date().getUTCDay()
-    const day =dayOfTheWeek(getDayToday)
-    const github_file_url = 'https://github.com/ogum-kevin/HngBackend/blob/main/server.js'
-    const github_repo_url = 'https://github.com/ogum-kevin/HngxBackend'
-
-    res.json({
-        slack_name:slackName,
-        current_day:day,
-        utc_time:UTC_time,
-        track:track,
-        github_file_url:github_file_url,
-        github_repo_url:github_repo_url,
-        status_code:200
-    })
-    //console.log('I have been triggered')
-    // console.log(UTC_time);
-   // console.log(day);
-})
+app.use('/api',userRoute);
+app.use(notFound);
 
 const PORT= process.env.PORT || 3000
 
